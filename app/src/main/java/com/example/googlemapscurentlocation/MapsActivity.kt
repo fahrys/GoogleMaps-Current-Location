3acca34b687d3182b0f78d2c7ac6d60a435d092c
fahrys <fahrisiaputra12@gmail.com>
2021-05-05 14:16:33 +0700

    Google Maps

--- app/src/main/java/com/example/googlemapscurentlocation/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapscurentlocation/MapsActivity.kt
@@ -1,35 +1,45 @@
 package com.example.googlemapscurentlocation
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity() {
 
     private lateinit var mMap: GoogleMap
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private lateinit var currentAddress:String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        getCurrentLocation()
     }
 
+    @SuppressLint("RestrictedApi")
+    fun getCurrentLocation() {
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
 
+        val locationRequest = LocationRequest ()
+                .setInterval(3000)
+                .setFastestInterval(3000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        val fusedLocationProviderClient = LocationServices
+                .getFusedLocationProviderClient(this)
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -38,12 +48,45 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this , arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            return
         }
-        mMap.isMyLocationEnabled = true
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest ,
+                object : LocationCallback(){
+                    override fun onLocationResult(p0: LocationResult) {
+                        super.onLocationResult(p0)
+                    for (location in p0.locations) {
+                        mapFragment.getMapAsync(OnMapReadyCallback {
+                            mMap = it
+                            if (ActivityCompat.checkSelfPermission(this@MapsActivity , Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this@MapsActivity , Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                                ActivityCompat.requestPermissions(this@MapsActivity , arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+                            }
+                            mMap.isMyLocationEnabled = true
+                            mMap.uiSettings.isZoomControlsEnabled = true
+                            val locationResult = LocationServices.getFusedLocationProviderClient(this@MapsActivity).lastLocation
+                            locationResult.addOnCompleteListener(this@MapsActivity ) {
+                                if (it.isSuccessful && it.result != null) {
+
+                                    var currentLocation = it.result
+                                    var currentLatitude = currentLocation.latitude
+                                    var currentLogtitude = currentLocation.longitude
+                                    val geocoder = Geocoder(this@MapsActivity)
+                                    var geocoderresult = geocoder.getFromLocation(currentLatitude , currentLogtitude , 1)
+                                    currentAddress = geocoderresult[0].getAddressLine(0)
+                                    var myLocation = LatLng(currentLatitude, currentLogtitude,)
+                                    mMap.addMarker(MarkerOptions().position(myLocation).title(currentAddress)).showInfoWindow()
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation))
+                                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15f))
+                                }
+                            }
+
+                        })
+
+                    }
+                    }
+                },
+                Looper.myLooper()
+        )
     }
 }
